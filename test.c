f2df39be9339b9202c761a426e0a58ce894f83c4
Solo502 <hoa.nguyen.minh@hcmut.edu.vn>
2024-09-01 01:06:15 +0700

    update remote

--- test.c
+++ test.c
@@ -2,11 +2,10 @@
 
 int main() {
     int a = 1, b = 3;
-    printf("%d",a-b);
-    printf("%d",a-b);
-    printf("%d",a-b);
-    printf("%d",a/b);
     printf("%d",a+b);
+    printf("%d",a-b);
+    printf("%d",a*b);
+    printf("%d",a/b);
 
     return 0;
 }
